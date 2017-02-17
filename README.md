c2477568976045eca44a0b50ddb48f0d34bda036
dominikspeh <mail@domi-speh.de>
2017-02-17 09:38:40 +0100

    Implemented stuttgarter nachrichten vfb rss feed

---
+++ README.md
@@ -0,0 +1,2 @@
+##Dude Telegram Chatbot
+**by Dominik Speh based on Node.js**
--- dudeBot.js
+++ dudeBot.js
@@ -5,46 +5,110 @@ const vfb = require('./vfb');
 
 dotenv.load({ path: '.env' });
 
-console.log(vfb.loadFeed());
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TelegramAPI;
 
-// Create a bot that uses 'polling' to fetch new updates
-const robot = new Bot(token, { polling: true });
+// Create bot
+const dude = new Bot(token, { polling: true });
 
-// Matches "/echo [whatever]"
-robot.onText(/\/echo (.+)/, function (msg, match) {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+// News Counter
+const user = [];
 
-    var chatId = msg.chat.id;
-    var resp = match[1]; // the captured "whatever"
 
-    // send back the matched "whatever" to the chat
-    robot.sendMessage(chatId, resp);
-});
 
-robot.onText(/\/vfbnews/, (msg) => {
-    const chatId = msg.from.id;
+dude.onText(/\/vfbnews/, (msg) => {
+    var chatId = msg.from.id;
+
+    var newUser = {};
+    newUser[chatId] = {
+        vfbnews : 0
+
+    };
+    user.push(newUser)
+
+    dude.sendMessage(chatId, "Ok! Hier ein paar News vom VfB Stuttgart");
+
 
 
     const options = {
         parse_mode: 'HTML',
-        disable_web_page_preview: true
+        disable_web_page_preview: true,
+        reply_markup: JSON.stringify({
+            keyboard: [
+                ["Show me more"],
+                ["That's enough"],
+            ]
+        })
     };
 
     vfb.loadFeed().then(data => {
-        robot.sendMessage(chatId, "Ok! Hier ein paar News vom VfB Stuttgart");
 
         const title = data.feed.items[0].title;
         const description = data.feed.items[0].description.replace(/<\/?[^>]+(>|$)/g,"").trim();
         const link = data.feed.items[0].link;
 
-        robot.sendMessage(chatId, "<b>"+title+"</b>\n"+description+"\n\n<a href='"+link+"'>Artikel lesen</a>", options);
+        dude.sendMessage(chatId, "<b>"+title+"</b>\n"+description+"\n\n<a href='"+link+"'>Artikel lesen</a>", options);
 
     });
+
+});
+
+// REACT VFB NEWS FEED
+dude.onText(/\Show me more/, (msg) => {
+    const chatId = msg.from.id;
+    user[0][chatId].vfbnews = user[0][chatId].vfbnews +1;
+
+    const newIndex = user[0][chatId].vfbnews
+
+    dude.sendMessage(chatId, "Nice! Hier weitere News vom VfB Stuttgart");
+
+    const options = {
+        parse_mode: 'HTML',
+        disable_web_page_preview: true,
+        reply_markup: JSON.stringify({
+            keyboard: [
+                ["Show me more"],
+                ["That's enough"],
+            ]
+        })
+    };
+
+    vfb.loadFeed().then(data => {
+
+        const title = data.feed.items[newIndex].title;
+        const description = data.feed.items[newIndex].description.replace(/<\/?[^>]+(>|$)/g,"").trim();
+        const link = data.feed.items[newIndex].link;
+
+        dude.sendMessage(chatId, "<b>"+title+"</b>\n"+description+"\n\n<a href='"+link+"'>Artikel lesen</a>", options);
+
+    });
+
+});
+
+dude.onText(/\That's enough/, (msg) => {
+    const chatId = msg.from.id;
+    const options = {
+        parse_mode: 'HTML',
+        disable_web_page_preview: true,
+        reply_markup: JSON.stringify({
+            remove_keyboard: true
+        })
+    };
+
+    if ( user[0][chatId] =! undefined){
+        user[0][chatId].vfbnews = 0;
+        dude.sendMessage(chatId, "Ok, das wars", options);
+
+    }
+    else {
+        dude.sendMessage(chatId, "", options);
+
+    }
+
+
+
+
+
 
 });
 
